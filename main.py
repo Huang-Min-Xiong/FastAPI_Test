bd03eecf23f8ef202dbd2e723d43cee860409d04
黃敏雄 <48427735+Huang-Min-Xiong@users.noreply.github.com>
2021-06-12 01:01:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"msg": "HelloWorld"}
+    return {"msg": "Hello World"}
 
 
 @app.get("/items/{item_id}/{user_name}")
 def read_item(item_id: int, user_name: str):
     return {"item_id": item_id, "user_name": user_name}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
